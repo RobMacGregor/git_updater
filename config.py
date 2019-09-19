96d17e60c8ea2c6f63857d25742cab599cb66137
RobMacGregor <robmac8138@gmail.com>
2019-09-19 12:39:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 repo_owner = 'RobMacGregor'
 repo_name = 'esp32_basic'
-essid = 'OUR'
-password = 'Micropython'
+essid = 'NETGEAR21
+password = 'Riverview'
 
 with open('.gitignore', 'w') as f:
 	f.write(repo_name)
